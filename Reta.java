8d6912ea4a919a38b2a3bdb64a065194fe0c9c63
Luiz Henrique <luizhenriquesilverios@hotmail.com>
2017-03-02 10:41:48 -0300

    Adicionado cálculo de área em "Reta".

--- Reta.java
+++ Reta.java
@@ -13,3 +13,23 @@ public class Reta{
 		return this.p2;
 	}
 }
+
+	public double sobarea(){
+	double x1, x2, y1, y2;
+	x1 = this.getP1().getX();
+	x2 = this.getP2().getX();
+	y1 = this.getP1().getY();
+	y2 = this.getP2().getY();
+	double a1, a2, x3, y3;
+	x3 = (x1-x2);
+	x3 = Math.abs(x3);
+	y3 = (y1-y2);
+	y3 = Math.abs(y3);
+	a1 = (x3*y3)/2;
+	if(y1>y2)
+		a2 = x3*y2;
+	else
+		a2 = x3*y1;
+		double a3 = a1+a2;
+		return a3;
+	}
