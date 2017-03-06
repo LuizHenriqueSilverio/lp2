40668936e751e67fee5f23f618acf867babef7ee
Luiz Henrique <luizhenriquesilverios@hotmail.com>
2017-03-05 21:36:53 -0300

    Finalizados os arquivos "Lista" e "Principal".

--- EP1/Lista.java
+++ EP1/Lista.java
@@ -1,33 +1,79 @@
 	class No{
 		private:		
-			int x;
+			int num;
 			No prox;
 			No ant;
 			No cabeca;
 			No fim;
-			int cont = 0;
 		public:
-			void InserirInt(int y){
-				this.x = y;
+			void InserirInt(int x){
+				this.num = y;
 			}
 			void LigarProx(No proximo){
 				this.prox = proximo;
 			}
 			void LigarAnt(No anterior){
 				this.ant = anterior;
-			}	
+			}
+			void RetornarFim(){
+				return fim;
+			}
 	}
 public class Lista{
 	public:	
-		void Inserir(){
+		void Inserir(int x){
 			No novo = new No();
-			if(ant = null){
+			if(cabeca = null){
 				cabeca = novo;
 				fim = novo;
 				novo.InserirInt(x);
-				cont = cont+1;
 			}
-			
+			if(cabeca != null){
+				novo.InserirInt(x);
+				novo.LigarAnt(No.RetornarFim());
+				novo.ant.prox = LigarProx(novo);
+				novo = fim;
+			}
+		}
+		bool Pesquisar(int y){
+			No cursor = cabeca;
+			while(prox != null){
+				if(cursor.num == y){
+					System.out.println("Número encontrado!");
+					return true;
+				}else{
+					cursor = cursor.prox;
+				}
+			}
+			System.out.println("Número não encontrado.");
+			return false;
+		}	
+		void Remover(int x){
+			if(Lista.Pesquisar(x) == false){
+				System.out.println("Não existe este número na Lista!");
+				break;
+			}
+			No cursor = cabeca;
+			if(cursor.x = cabeca){
+				No aux = cursor.prox;
+				aux = cabeca;
+				delete[] cursor;
+				break;
+			}
+			while(prox != null){
+				if(cursor.num = x){
+					No aux1 = cursor.ant;
+					No aux2 = cursor.prox;
+					aux1.LigarProx(aux2);
+					aux2.LigarAnt(aux1);
+					break;
+				}
+				cursor = cursor.prox;
+			}
+			No aux = cursor.ant;
+			aux = fim;
+			delete[] cursor;
+		}		
 															
 		}
 			
--- EP1/Principal.java
+++ EP1/Principal.java
@@ -1,5 +1,11 @@
 public class Principal{
 	public static void main(String[] args){
-			
+		Lista L1;
+		L1.Inserir(58);
+		L1.Inserir(44);
+		L1.Inserir(898);
+		L1.Procurar(456);
+		L1.Procurar(44);
+		L1.Remover(44);
 	}
 }
