8bf0a8dead4af33fc53d3ac3b1543ffa2515e948
Luiz Henrique <luizhenriquesilverios@hotmail.com>
2017-03-08 11:09:52 -0300

    Pasta "ep3" adicionada.

---
+++ ep3/Aluno.java
@@ -0,0 +1,14 @@
+public class Aluno{
+	private String nome;
+	private int idade;
+	public void setAluno(String x, int y){
+		this.nome = x;
+		this.idade = y;
+	}
+	public String getName(){
+		return nome;
+	}
+	public int getIdade(){
+		return idade;
+	}
+}
---
+++ ep3/Principal.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+public class Principal{
+	public static void main(String[] args){
+		ArrayList<Aluno>alunos = new ArrayList<Aluno>();
+		Aluno aluno1 = new Aluno();
+		aluno1.setAluno("Luiz Henrique", 17);
+		alunos.add(aluno1);
+		Aluno aluno2 = new Aluno();
+		aluno2.setAluno("Gabriel Monteiro", 16);
+		alunos.add(aluno2);
+		Aluno aluno3 = new Aluno();
+		aluno3.setAluno("Elyas Correa", 18);
+		alunos.add(aluno3);
+		Aluno aluno4 = new Aluno();
+		aluno4.setAluno("Wagner Aristides", 13);
+		alunos.add(aluno4);
+		Aluno aluno5 = new Aluno();
+		aluno5.setAluno("Jailson Mendes", 30);
+		alunos.add(aluno5);
+		for(int i; i = 0; i++){
+			System.out.println()
+		}
+	}
+}
