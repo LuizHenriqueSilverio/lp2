8f8516e30c3b54e336e76c733ee16ee42367a796
Luiz Henrique <luizhenriquesilverios@hotmail.com>
2017-03-08 11:16:16 -0300

    "ep3" Atualizado.

--- ep3/Aluno.java
+++ ep3/Aluno.java
@@ -1,6 +1,7 @@
-public class Aluno{
+public class Aluno implements Comparable{
 	private String nome;
 	private int idade;
+
 	public void setAluno(String x, int y){
 		this.nome = x;
 		this.idade = y;
@@ -10,5 +11,10 @@ public class Aluno{
 	}
 	public int getIdade(){
 		return idade;
+	}
+
+	public int compareTo(Aluno comparesTo){
+		int compareidade=((Aluno)compareTo).getIdade();
+		return this.idade-compareidade;
 	}
 }
